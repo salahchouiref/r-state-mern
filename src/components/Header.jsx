023746bf6d0744e04fca3075d2cbd2b5cced5968
salahchouiref <popomaha7@gmail.com>
2023-11-09 16:09:43 +0100

    header component & UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {FaSearch} from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-violet-200 shadow-md'>
+     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to="/">
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-violet-400'>Sech</span>
+                <span className='text-violet-800'>R-state</span>
+            </h1>
+        </Link>
+        <form className='bg-violet-100 p-3 rounded-lg flex items-center'>
+            <input type='text' placeholder='search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+            <FaSearch className='text-violet-500' />
+        </form>
+        <ul className='flex gap-4'>
+            <Link to="/">
+                <li className='hidden sm:inline hover:underline cursor-pointer'>Home</li>
+            </Link>
+            <Link to="/about">
+                <li className='hidden sm:inline hover:underline cursor-pointer'>About</li>
+            </Link>
+            <Link to="/sign-in">
+                <li className='hover:underline cursor-pointer'>Sign in</li>
+            </Link>
+        </ul>
+     </div>
+    </header>
+  )
+}
