f4ee8b5c431aa6e67539bc7f21d096535da6fe9a
salahchouiref <popomaha7@gmail.com>
2023-11-10 15:52:21 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+const databaseConnection = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_LINK).then(()=>{
+            console.log("connected to mongodb");
+        })
+    }catch(err){
+        console.log(err);
+    }
+}; 
+databaseConnection();
 
 app.listen(3000,()=>{
     console.log("app is running on : http://localhost:3000");
