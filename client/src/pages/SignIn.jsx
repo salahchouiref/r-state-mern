645c8f2bf605aaec462cb4ef76d30a76e8160bdd
salahchouiref <popomaha7@gmail.com>
2023-11-13 16:08:59 +0100

    complete signIn page with full functionnality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,105 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formdata, setFormData] = useState({});
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formdata, [e.target.id]: e.target.value });
+    setErrors({ ...errors, [e.target.id]: "" });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // Frontend validation
+    const validationErrors = {};
+
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!formdata.email || !emailRegex.test(formdata.email.trim())) {
+      validationErrors.email = "Please enter the email address";
+    }
+
+    // Minimum length for password
+    if (!formdata.password || formdata.password.trim().length < 6) {
+      validationErrors.password = `Password is too short!`;
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formdata),
+      });
+
+      const data = await res.json();
+      setLoading(false);
+
+      if (data.success === false) {
+        setErrors({ server: data.message });
+        return;
+      }
+      navigate("/profile");
+    } catch (err) {
+      setLoading(false);
+      setErrors({ server: err.message });
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        {/* Remove the username input field */}
+        <input
+          type="email"
+          className={`bg-violet-50 p-3 rounded-lg ${errors.email ? "border-red-500" : ""}`}
+          placeholder="email"
+          id='email'
+          onChange={handleChange}
+        />
+        {errors.email && (
+          <p className="text-red-600 mt-1 text-sm">{errors.email}</p>
+        )}
+        <input
+          type="password"
+          className={`bg-violet-50 p-3 rounded-lg ${errors.password ? "border-red-500" : ""}`}
+          placeholder="password"
+          id='password'
+          onChange={handleChange}
+        />
+        {errors.password && (
+          <p className="text-red-600 mt-1 text-sm">{errors.password}</p>
+        )}
+        <button
+          disabled={loading ? true : false}
+          className="bg-violet-900 text-white p-2 rounded-l uppercase hover:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign up"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-2 ">
+        <p>You don't have an account?</p>
+        <Link to="/sign-up">
+          <span className='text-blue-600'>Sign up</span>
+        </Link>
+      </div>
+      {errors.server && (
+        <p className="text-red-600 mt-3 text-sm">{errors.server}</p>
+      )}
+    </div>
+  );
 }
