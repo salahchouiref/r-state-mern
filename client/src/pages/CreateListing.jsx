c8b689c8d139612867cad18144fcb11d87703d09
salahchouiref <popomaha7@gmail.com>
2023-11-21 16:58:02 +0100

    fix upload images logique and functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -9,6 +9,19 @@ export default function CresetateListing() {
   }); 
   const [imageUploadError,setImageUploadError] = useState(null);
   const [uploading,setUploading] = useState(false);
+  const [uploadingSuccess,setUploadingSuccess] = useState(false);
+  const [imagePreviews, setImagePreviews] = useState([]);
+
+  const handleImageChange = (e) => {
+    const selectedFiles = e.target.files;
+    setFiles(selectedFiles);
+
+    // Create image previews for selected files
+    const previews = Array.from(selectedFiles).map((file) => URL.createObjectURL(file));
+    setImagePreviews(previews);
+  };
+
+
   const handleImageSubmit = (e) =>{
     if(files.length>0 && files.length + formData.imageUrls.length <7){
       setUploading(true);
@@ -21,13 +34,18 @@ export default function CresetateListing() {
         setFormData({...formData,imageUrls:formData.imageUrls.concat(urls)});
         setImageUploadError(false);
         setUploading(false);
+        setUploadingSuccess(true);
+        setFiles([]);
+        setImagePreviews([]);
       }).catch((err)=>{
         setImageUploadError("Image upload failed (2mb max per image)");   
         setUploading(false);
+        setUploadingSuccess(false);
       });
     }else{
-      setImageUploadError("You can update at least 1 to 6 images per listing");
+      setImageUploadError("You can upload at least 1 to 6 images per listing");
       setUploading(false);
+      setUploadingSuccess(false);
     }
   };
   
@@ -61,7 +79,15 @@ export default function CresetateListing() {
     setFormData({
       ...formData,
       imageUrls : formData.imageUrls.filter((_,i)=>i!==index),
-    })
+    });
+    const updatedPreviews = [...imagePreviews];
+    updatedPreviews.splice(index, 1);
+    setImagePreviews(updatedPreviews);
+    
+    const updatedFiles = [...files];
+    updatedFiles.splice(index, 1);
+    setFiles(updatedFiles);
+    console.log(files);
   };
 
   return (
@@ -119,27 +145,60 @@ export default function CresetateListing() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col flex-1 gap-4 ">
-          <p className='font-semibold'>Images : 
-            <span className='font-normal text-gray-600 ml-2'>The first image will be the cover</span>
-          </p>
-          <div className=" flex gap-4">
-            <input onChange={(e)=>setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type='file' id='images' accept='image/*' multiple />
-            <button type='button' onClick={handleImageSubmit} className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>
-              { uploading ? "...Uploading" : "Upload"}
-            </button>
-          </div>
+        <div className="flex flex-col flex-1 gap-4">
+        <p className='font-semibold'>Images :
+          <span className='font-normal text-gray-600 ml-2'>The first image will be the cover</span>
+        </p>
+        <div className="flex gap-4">
+          <input
+            onChange={handleImageChange}
+            className='p-3 border border-gray-300 rounded w-full'
+            type='file'
+            id='images'
+            accept='image/*'
+            multiple
+            disabled={uploadingSuccess}
+          />
+          <button
+            type='button'
+            onClick={handleImageSubmit}
+            className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'
+            disabled={uploading || uploadingSuccess}
+          >
+            {uploading ? "...Uploading" : "Upload"}
+          </button>
+        </div>
         <p className='text-red-700 text-sm font-semibold'>{imageUploadError && imageUploadError}</p>
         {
-          formData.imageUrls.length > 0 && formData.imageUrls.map((url,index)=>{
-            return (<div key={index} className='flex justify-between p-2 border items-center '>
-              <img src={url} alt='listing image' className='w-40 h-40 object-contain rounded-lg'/>
-              <button type='button' onClick={()=>handleRemoveImage(index)} className='p-3 text-red-700 rounded-lg uppercase hover:opacity-90'>Delete</button>
-            </div>)
-          }) 
+          uploadingSuccess && <p className='text-green-700 text-sm font-semibold'>
+          Images was saved successfuly
+          <span className='text-yellow-300 text-xs font-semibold mx-3'>  (You can't cancel this opration)</span>
+        </p>
         }
-        <button className='p-3 bg-violet-700 text-white uppercase rounded-lg hover:opacity-80 disabled:opacity-70 '>Create Listing</button>
-        </div>
+        
+          {imagePreviews.map((preview, index) => (
+            <div key={index} className='flex justify-between p-2 border items-center'>
+              <img src={preview} alt={`preview-${index}`} className='w-40 h-40 object-contain rounded-lg' />
+              <div className='flex items-center flex-col'>
+                <p className='text-gray-600 text-xs mt-2'>
+                  Size: {(files[index].size / (1024 * 1024)).toFixed(2)} MB
+                </p>
+                <p className='text-gray-600 text-xs'>
+                  File: {files[index].name.length > 15 ? files[index].name.substring(0, 15) + '...' + files[index].name.split('.').pop() : files[index].name}
+                </p>
+              </div>
+              <button
+                type='button'
+                onClick={() => handleRemoveImage(index)}
+                className='p-3 text-red-700 rounded-lg uppercase hover:opacity-90'
+                disabled={uploading}
+              >
+                Delete
+              </button>
+            </div>
+          ))}
+          <button className='p-3 bg-violet-700 text-white uppercase rounded-lg hover:opacity-80 disabled:opacity-70 '>Create Listing</button>
+      </div>
       </form>
     </main>
   )
