bb49a01f946cad7d8c7e3d366b9eca079db7db44
salahchouiref <popomaha7@gmail.com>
2023-12-03 16:04:31 +0100

    Fix token duration

--- api/utils/token.js
+++ api/utils/token.js
@@ -3,6 +3,6 @@ import jwt from "jsonwebtoken";
 export const createToken = (user) => {
     const token = jwt.sign({id:user._id},process.env.TOKEN_CODE);
     const {password : hashedPassword,...rest} = user._doc;
-    const expiryDate = new Date(Date.now()+3600000);
+    const expiryDate = new Date(Date.now()+3600000*24);
     return {rest,token,expiryDate};
 }
