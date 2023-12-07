7e87f3906b97538a3a10449d06cc9f300431994d
salahchouiref <popomaha7@gmail.com>
2023-12-07 15:52:48 +0100

    complete about page ui and fix home page ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,8 +10,9 @@ export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
   const [saleListings, setSaleListings] = useState([]);
   const [rentListings, setRentListings] = useState([]);
+  
   SwiperCore.use([Navigation]);
-  console.log(offerListings);
+
   useEffect(() => {
     const fetchOfferListings = async () => {
       try {
@@ -23,6 +24,7 @@ export default function Home() {
         console.log(error);
       }
     };
+
     const fetchRentListings = async () => {
       try {
         const res = await fetch('/api/listing/get?type=rent&limit=4');
@@ -40,95 +42,76 @@ export default function Home() {
         const data = await res.json();
         setSaleListings(data);
       } catch (error) {
-        log(error);
+        console.error(error);
       }
     };
+
     fetchOfferListings();
   }, []);
+
   return (
     <div>
-      {/* top */}
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
-        <h1 className='text-stone-700 font-bold text-3xl lg:text-6xl'>
-          Find your next <span className='text-stone-500'>perfect</span>
-          <br />
-          place with ease
+      {/* Top Section */}
+      <div className='bg-gray-100 py-20 px-4 text-center'>
+        <h1 className='text-4xl lg:text-6xl font-bold text-gray-800 mb-4'>
+          Find your next <span className='text-blue-500'>perfect</span> place with ease
         </h1>
-        <div className='text-gray-400 text-xs sm:text-sm'>
-          Sech Estate is the best place to find your next perfect place to
-          live.
+        <p className='text-gray-600 text-lg mb-6'>
+          Sech Estate is the best place to find your next perfect place to live.
           <br />
           We have a wide range of properties for you to choose from.
-        </div>
+        </p>
         <Link
-          to={'/search'}
-          className='text-xs sm:text-sm text-blue-800 font-bold hover:underline'
+          to='/search'
+          className='text-sm md:text-base text-blue-800 font-bold hover:underline'
         >
           Let's get started...
         </Link>
       </div>
 
-      {/* swiper */}
+      {/* Swiper Section */}
       <Swiper navigation>
         {offerListings &&
           offerListings.length > 0 &&
           offerListings.map((listing) => (
-            <SwiperSlide>
+            <SwiperSlide key={listing._id}>
               <div
                 style={{
                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
                   backgroundSize: 'cover',
+                  height: '500px',
                 }}
-                className='h-[500px]'
-                key={listing._id}
               ></div>
             </SwiperSlide>
           ))}
       </Swiper>
 
-      {/* listing results for offer, sale and rent */}
-
-      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 my-10'>
-        {offerListings && offerListings.length > 0 && (
-          <div className=''>
-            <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent offers</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?offer=true'}>Show more offers</Link>
-            </div>
-            <div className='flex flex-wrap gap-4'>
-              {offerListings.map((listing) => (
-                <ListingItem listing={listing} key={listing._id} />
-              ))}
-            </div>
-          </div>
-        )}
-        {rentListings && rentListings.length > 0 && (
-          <div className=''>
-            <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for rent</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=rent'}>Show more places for rent</Link>
-            </div>
-            <div className='flex flex-wrap gap-4'>
-              {rentListings.map((listing) => (
-                <ListingItem listing={listing} key={listing._id} />
-              ))}
-            </div>
-          </div>
-        )}
-        {saleListings && saleListings.length > 0 && (
-          <div className=''>
-            <div className='my-3'>
-              <h2 className='text-2xl font-semibold text-slate-600'>Recent places for sale</h2>
-              <Link className='text-sm text-blue-800 hover:underline' to={'/search?type=sale'}>Show more places for sale</Link>
-            </div>
-            <div className='flex flex-wrap gap-4'>
-              {saleListings.map((listing) => (
-                <ListingItem listing={listing} key={listing._id} />
-              ))}
-            </div>
-          </div>
-        )}
+      {/* Listing Results Section */}
+      <div className='container mx-auto py-10 '>
+        {renderListings("Recent offers", offerListings, '/search?offer=true')}
+        {renderListings("Recent places for rent", rentListings, '/search?type=rent')}
+        {renderListings("Recent places for sale", saleListings, '/search?type=sale')}
       </div>
     </div>
   );
 }
+
+// Helper function to render listing sections
+const renderListings = (title, listings, link) => {
+  return (
+    <div className='mb-8 mx-5'>
+      <div className='mb-3'>
+        <h2 className='text-2xl font-semibold text-gray-800'>{title}</h2>
+        <Link className='text-sm text-blue-800 hover:underline' to={link}>
+          Show more
+        </Link>
+      </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
+        {listings && listings.length > 0 &&
+          listings.map((listing) => (
+            <ListingItem key={listing._id} listing={listing} />
+          ))}
+      </div>
+    </div>
+  );
+};
